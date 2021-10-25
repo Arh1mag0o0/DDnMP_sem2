ca982584df8a7776bc3040c2adb67d635a38cb20
Andrey_Ableev <ableevv.an@yandex.ru>
2021-10-25 10:58:06 +0700

    убрал строчку теперь работает с *.с файлом

--- Objects/main.d
+++ Objects/main.d
@@ -2,4 +2,3 @@
 .\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
 .\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 .\objects\main.o: functions.h
-.\objects\main.o: functions.c
